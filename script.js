e61aeae9db56dbd7c7627883feb2f8d255decd46
maciejbikprz <177043@stud.prz.edu.pl>
2025-10-28 10:31:34 +0000

    Com2_1.3.

--- script.js
+++ script.js
@@ -15,27 +15,58 @@
   })
 
   cw1.addEventListener("click", function() {
+    fetchGET();
+  });
+
+  function fetchGET(url) {
     answer.innerHTML = "Loading...";
+    if (url == null) {
+      url = "https://jsonplaceholder.typicode.com/posts"; // Domyślny URL dla wielu postów
+    }
+
     setTimeout(function() {
       answer.innerHTML = "";
       var list = answer.appendChild(document.createElement("ul"));
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json()).then(array => (array.forEach(element => {
-          var item = list.appendChild(document.createElement("li"));
-          item.innerHTML =
-            `<div>
-            <b>ID: ${element.id}</b>
-            <p><u>Title:</u><br> ${element.title}</p>
-            <p><u>Body:</u><br> ${element.body}</p>
-            <p><u>User ID:</u><br> ${element.userId}</p>
-            </div>`
 
-        })))
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          if (Array.isArray(data)) {
+            data.forEach(element => {
+              var item = list.appendChild(document.createElement("li"));
+              item.innerHTML = `
+                <div>
+                  <b>ID: ${element.id}</b>
+                  <p><u>Title:</u><br> ${element.title}</p>
+                  <p><u>Body:</u><br> ${element.body}</p>
+                  <p><u>User ID:</u><br> ${element.userId}</p>
+                </div>
+              `;
+            });
+          } else {
+            var item = list.appendChild(document.createElement("li"));
+            item.innerHTML = `
+              <div>
+                <b>ID: ${data.id}</b>
+                <p><u>Title:</u><br> ${data.title}</p>
+                <p><u>Body:</u><br> ${data.body}</p>
+                <p><u>User ID:</u><br> ${data.userId}</p>
+              </div>
+            `;
+          }
+        })
+        .catch(error => {
+          answer.innerHTML = "Error fetching data.";
+          console.error(error);
+        });
+
     }, 2000);
+  }
 
-
-
-  })
+  var select1 = document.getElementById("select1");
+  select1.addEventListener("change", function() {
+    fetchGET("https://jsonplaceholder.typicode.com/posts/" + select1.value);
+  });
 
   cw2.addEventListener("click", function() {
     //TODO
